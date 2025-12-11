d22b04e2214121d14f52cc626d6f992778f3bf15
Anuradha Malkapure <152046058+anuradha-09@users.noreply.github.com>
2025-12-10 22:21:02 -0500

    Delete Models directory

--- Models/Supplier.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace Inventory_Management_System.Models;
-public class Supplier
-{
-    public int SupplierId { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public ICollection<Product> Products { get; set; }
-}
